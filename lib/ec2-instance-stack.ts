342ed6a676fbaa6d2fe40360d7d1e7feb139064c
non-97 <nonmonaka07@gmail.com>
2022-06-09 13:21:26 +0900

    initial commit

--- lib/ec2-instance-stack.ts
+++ lib/ec2-instance-stack.ts
@@ -1,16 +1,68 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, aws_ec2 as ec2 } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class Ec2InstanceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, "VPC", {
+      cidr: "10.10.0.0/24",
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      natGateways: 0,
+      maxAzs: 2,
+      subnetConfiguration: [
+        { name: "Public", subnetType: ec2.SubnetType.PUBLIC, cidrMask: 28 },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ec2InstanceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // EC2 Instance
+    new ec2.Instance(this, "EC2 Instance Amazon Linux 2", {
+      instanceType: new ec2.InstanceType("t3.micro"),
+      machineImage: ec2.MachineImage.latestAmazonLinux({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      vpc: vpc,
+      blockDevices: [
+        {
+          deviceName: "/dev/xvda",
+          volume: ec2.BlockDeviceVolume.ebs(8, {
+            volumeType: ec2.EbsDeviceVolumeType.GP3,
+          }),
+        },
+      ],
+      propagateTagsToVolumeOnCreation: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    });
+
+    new ec2.Instance(this, "EC2 Instance Windows Server 2022", {
+      instanceType: new ec2.InstanceType("t3.micro"),
+      machineImage: ec2.MachineImage.latestWindows(
+        ec2.WindowsVersion.WINDOWS_SERVER_2022_ENGLISH_FULL_BASE
+      ),
+      vpc: vpc,
+      blockDevices: [
+        {
+          deviceName: "/dev/sda1",
+          volume: ec2.BlockDeviceVolume.ebs(30, {
+            volumeType: ec2.EbsDeviceVolumeType.GP3,
+          }),
+        },
+      ],
+      propagateTagsToVolumeOnCreation: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    });
+
+    // Enable EBS Optimized
+    this.node.children.forEach((child) => {
+      if (child.node.defaultChild instanceof ec2.CfnInstance) {
+        child.node.defaultChild.ebsOptimized = true;
+      }
+    });
   }
 }
